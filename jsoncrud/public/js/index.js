a3d9af3417f09ba6996b1724dfe209fc1cff371f
Ashish Amar <ashishamar1999@gmail.com>
2021-08-19 11:07:51 +0530

    JSON Crud Done.

--- jsoncrud/public/js/index.js
+++ jsoncrud/public/js/index.js
@@ -1,7 +1,91 @@
+var url = "http://localhost:3000/dept";
+
 async function getDept(){
-    fetch("http://localhost:3000/dept").then((res)=>{
-        return res.json();
+    fetch(url).then((response)=>{
+        return response.json();
+    
     }).then((dept)=>{
-        console.log(dept);
+
+        console.table(dept);
+    
     })
+
+}
+
+async function addDept(){
+
+    /******************************************************** 
+    NOTE - HEADER TYPE IS A MUST WHEN MAKING A FETCH REQUEST!
+    ********************************************************/ 
+    
+    var id = Number(prompt("Enter ID"));
+    var dname = prompt("Enter Department Name");
+    var loc = prompt("Enter Location");
+
+    var newRecord = {
+        "id": id,
+        "dname": dname,
+        "loc": loc
+    }
+
+    console.log(newRecord);
+
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newRecord),
+        headers:{
+            'Content-Type':'application/json' 
+        },
+    }).then((response) => {
+        console.warn(response);
+        return response.json();
+    }).then((returnresult) => {
+        console.log(returnresult);
+    })
+}
+
+async function updateDept(){
+    var id = Number(prompt("Enter Modified ID"));
+    var dname = prompt("Enter Modified Department Name");
+    var loc = prompt("Enter Modified Location");
+
+    var newRecord = {
+        "id": id,
+        "dname": dname,
+        "loc": loc
+    }
+
+    newurl = url;
+    newurl += `/${id}`;
+
+    fetch(newurl, {
+        method: "PUT",
+        body: JSON.stringify(newRecord),
+        headers:{
+            'Content-Type':'application/json' 
+        },
+    }).then((response) => {
+        console.warn(response);
+        return response.json();
+    })
+
+}
+
+async function deleteDept(){
+
+    var id = Number(prompt("Enter The ID of the record to be deleted."));
+
+    newurl = url;
+    newurl += `/${id}`;
+
+    fetch(newurl, {
+        method: "DELETE",
+        headers:{
+            'Content-Type':'application/json' 
+        },
+    }).then((response) => {
+        console.warn(response);
+        return response.json();
+    })
+
 }
