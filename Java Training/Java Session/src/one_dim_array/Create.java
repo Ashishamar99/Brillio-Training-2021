816ac5f0be8656d2c8849fe30726c9011d9be500
Ashish Amar <ashishamar1999@gmail.com>
2021-08-26 15:31:25 +0530

    1D Array Creation modified

--- Java Training/Java Session/src/one_dim_array/Create.java
+++ Java Training/Java Session/src/one_dim_array/Create.java
@@ -1,7 +1,16 @@
 package one_dim_array;
 
+
 public class Create extends ReadSize {
 	public static int[] numbers;
+	
+//	public static int[] copyArray;
+//	
+//	public Create() {
+//		copyArray = new int[numbers.length];
+//		copyArray = numbers.clone();
+//	}
+	
 	public void createArray() {
 		numbers = new int[n];
 	}
