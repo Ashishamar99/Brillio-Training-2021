7da1256a799c5a823791358ef6f23f9528b03709
Ashish Amar <ashishamar1999@gmail.com>
2021-09-07 10:12:12 +0530

    Using spring boots findById().

--- Java Training/Spring Boot/springbootdemo/src/main/java/com/brillio/springbootdemo/MyRestController.java
+++ Java Training/Spring Boot/springbootdemo/src/main/java/com/brillio/springbootdemo/MyRestController.java
@@ -2,10 +2,12 @@ package com.brillio.springbootdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MyRestController {
@@ -25,5 +27,12 @@ public class MyRestController {
         return studentDAO.findAll();
         //findAll is a method which is automatically created to fetch the data from the database and show it.
         //Basically does all the work of getAllStudents() in jdbc_demo project.
+    }
+
+    @GetMapping("/student/{id}") // Here ID is a path variable, basically a variable value which is passed in the URL
+    public Optional<StudentDTO> getAnyStudent(@PathVariable String id){
+        return studentDAO.findById(id);
+        //Designs a query for finding by id all by itself.
+        //Optional returns a specified object or null if there is no matching record.
     }
 }
